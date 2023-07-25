a22f0a61c28a5ed04cfe39f203112d65959238c2
Shams <skm478@gmail.com>
2023-07-25 18:18:31 -0400

    does git track an empty folder test

---
+++ server/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+class User(db.Model):
+    pass
+
+class Inventory():
+    pass
+
+class Deck():
+    pass
+
+class Card():
+    pass
